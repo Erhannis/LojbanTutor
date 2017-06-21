6191a096ebcece9729e82f0ba298bb43ff694e3c
erhannis <Eyeillus@gmail.com>
2017-06-21 09:13:08 -0400

    New repo; tries to teach you Lojban by reading it outloud with definitions.

---
+++ app/src/main/java/com/erhannis/android/lojbantutor/MainActivity.java
@@ -0,0 +1,133 @@
+package com.erhannis.android.lojbantutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.erhannis.lojban.core.Dictionary;
+import com.erhannis.lojban.core.LearningManager;
+import com.erhannis.lojban.core.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+  protected EditText etText;
+
+  protected Button btnReload;
+  protected Button btnToBeginning;
+  protected Button btnBack;
+  protected Button btnPlayPause;
+  protected Button btnForward;
+  protected Button btnToEnd;
+
+  protected Dictionary mDictionary;
+  protected ReadingManager mReadingManager;
+  protected LearningManager mLearningManager; //TODO Hmm.  Not convinced about these interactions.
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    etText = (EditText)findViewById(R.id.etText);
+
+    btnReload = (Button)findViewById(R.id.btnReload);
+    btnToBeginning = (Button)findViewById(R.id.btnToBeginning);
+    btnBack = (Button)findViewById(R.id.btnBack);
+    btnPlayPause = (Button)findViewById(R.id.btnPlayPause);
+    btnForward = (Button)findViewById(R.id.btnForward);
+    btnToEnd = (Button)findViewById(R.id.btnToEnd);
+
+    mLearningManager = new LearningManager(); //TODO Allow save progress
+
+    //TODO Handle errors
+    try {
+      List<String> dictLines = readAllLines(getResources().openRawResource(R.raw.jbo_eng_cleaned));
+      mDictionary = new Dictionary(dictLines);
+
+      String alis = readAll(getResources().openRawResource(R.raw.alice));
+      mReadingManager = new ReadingManager(this, mDictionary, mLearningManager, alis);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    btnReload.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mReadingManager.stop();
+
+        String text = etText.getText().toString();
+        mReadingManager = new ReadingManager(MainActivity.this, mDictionary, mLearningManager, text);
+      }
+    });
+
+    btnToBeginning.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mReadingManager.backToBeginning();
+      }
+    });
+    btnBack.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mReadingManager.back();
+      }
+    });
+    btnPlayPause.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        switch (btnPlayPause.getText().toString()) {
+          case "|>":
+            mReadingManager.play();
+            btnPlayPause.setText("||");
+            break;
+          case "||":
+            mReadingManager.stop();
+            btnPlayPause.setText("|>");
+            break;
+        }
+      }
+    });
+    btnForward.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mReadingManager.forward();
+      }
+    });
+    btnToEnd.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mReadingManager.forwardToEnd();
+      }
+    });
+  }
+
+  // https://stackoverflow.com/a/2549222/513038
+  public static String readAll(InputStream stream) throws IOException {
+    BufferedReader r = new BufferedReader(new InputStreamReader(stream));
+    StringBuilder total = new StringBuilder();
+    String line;
+    while ((line = r.readLine()) != null) {
+      total.append(line).append('\n');
+    }
+    return total.toString();
+  }
+
+  // kinda https://stackoverflow.com/a/2549222/513038
+  public static List<String> readAllLines(InputStream stream) throws IOException {
+    BufferedReader r = new BufferedReader(new InputStreamReader(stream));
+    ArrayList<String> lines = new ArrayList<>();
+    String line;
+    while ((line = r.readLine()) != null) {
+      lines.add(line);
+    }
+    return lines;
+  }
+}
---
+++ app/src/main/java/com/erhannis/android/lojbantutor/ReadingManager.java
@@ -0,0 +1,130 @@
+package com.erhannis.android.lojbantutor;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.speech.tts.Voice;
+
+import com.erhannis.lojban.core.Dictionary;
+import com.erhannis.lojban.core.LearningManager;
+import com.erhannis.lojban.core.LojbanTextUtils;
+
+import java.io.File;
+import java.util.Locale;
+
+/**
+ * Created by erhannis on 6/21/17.
+ */
+
+public class ReadingManager {
+  protected static final Locale JBO = new Locale("jbo");
+  protected static final Locale EN_US = new Locale("en_US");
+
+  protected final Dictionary mDictionary;
+  protected final LearningManager mLearningManager;
+  protected final String[] mLines;
+  protected int mCurLine = 0;
+  protected boolean mPlaying = false;
+  protected TextToSpeech tts;
+
+  public ReadingManager(Context ctx, Dictionary dictionary, LearningManager learningManager, String text) {
+    this.mDictionary = dictionary;
+    this.mLearningManager = learningManager;
+    this.mLines = LojbanTextUtils.lojbanToSentences(text);
+
+    tts = new TextToSpeech(ctx, new TextToSpeech.OnInitListener() {
+      @Override
+      public void onInit(int status) {
+        System.out.println("tts init'd");
+
+        for (Locale l : tts.getAvailableLanguages()) {
+          System.out.println("locale " + l);
+        }
+
+        tts.setLanguage(JBO);
+
+        for (Voice v : tts.getVoices()) {
+          System.out.println("Voice " + v);
+        }
+
+        System.out.println("Current voice: " + tts.getVoice());
+
+        tts.setSpeechRate(0.5f);
+      }
+    }, "com.reecedunn.espeak");
+
+    tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+      @Override
+      public void onStart(String utteranceId) {
+
+      }
+
+      @Override
+      public void onDone(String utteranceId) {
+        if ("readLojban".equals(utteranceId)) {
+          if (mPlaying) {
+            mCurLine++;
+          }
+          playCurLine();
+        }
+      }
+
+      @Override
+      public void onError(String utteranceId) {
+
+      }
+    });
+  }
+
+  public void play() {
+    mPlaying = true;
+    playCurLine();
+  }
+
+  public void stop() {
+    mPlaying = false;
+    tts.stop();
+  }
+
+  public void back() {
+    //TODO Do
+  }
+
+  public void forward() {
+    //TODO Do
+  }
+
+  public void backToBeginning() {
+    //TODO Do
+  }
+
+  public void forwardToEnd() {
+    //TODO Do
+  }
+
+  protected void playCurLine() {
+    if (mCurLine < mLines.length) {
+      String line = mLines[mCurLine];
+      String[] words = LojbanTextUtils.sentenceToWords(line);
+
+      tts.speak("", TextToSpeech.QUEUE_FLUSH, null, "flushQueue");
+
+      for (int i = 0; i < words.length; i++) {
+        String word = words[i];
+        if (mLearningManager.shouldGiveMeaning(word)) {
+          tts.setLanguage(JBO);
+          tts.speak(word + " .", TextToSpeech.QUEUE_ADD, null, "readWord" + i);
+          tts.setLanguage(EN_US);
+          tts.setPitch(1.5f);
+          tts.speak(mDictionary.getMeaning(word) + " . .", TextToSpeech.QUEUE_ADD, null, "readMeaning" + i);
+          tts.setPitch(1.0f);
+          mLearningManager.meaningGiven(word); //TODO Should come after actually given?
+          //TODO Note that this may mess up the current sentence when hit back.
+        }
+      }
+
+      tts.setLanguage(JBO);
+      tts.speak(line, TextToSpeech.QUEUE_ADD, null, "readLojban");
+    }
+  }
+}
